9124052ed3246da28bdfc9818d88faa1bf14118f
Jhon_User <jf29@misena.edu.co>
2016-08-21 14:00:34 -0500

    ...

--- WEBAPP/Model/log.class.php
+++ WEBAPP/Model/log.class.php
@@ -5,10 +5,11 @@
       try {
 
         $pdo=Conexion::Abrirbd();
+
         $consul = "SELECT usuario.usu_cod, usuario.usu_nom, usuario.usu_nick, usuario.usu_pass, rol.rol_cod, rol.rol_nombre FROM usuario
                    INNER JOIN rol ON rol.rol_cod = usuario.rol_cod WHERE usuario.usu_nick = ?";
+                   
         $mostrar = $pdo->prepare($consul);
-
         $mostrar->execute(array($name));
         $dato=$mostrar->fetch(PDO::FETCH_BOTH);
         return $dato;
