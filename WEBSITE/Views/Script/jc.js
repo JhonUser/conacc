b434bc0f16aabd7c8115815d2b73ea4e634ae35a
Jhon_User <jf29@misena.edu.co>
2016-08-20 22:17:40 -0500

    ....

---
+++ WEBSITE/Views/Script/jc.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+    $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+        
