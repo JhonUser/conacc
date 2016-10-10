8df3f7d44da21a27b6fc9db640f1eff3e320c76d
JhonUser <jf29@misena.edu.co>
2016-10-10 00:28:11 -0500

    ...

--- WEBAPP/Views/inicio.php
+++ WEBAPP/Views/inicio.php
@@ -1,2 +1,1 @@
 
-<center>..<br>...<br>..</center>
--- WEBAPP/Views/section.inicio.php
+++ WEBAPP/Views/section.inicio.php
@@ -1,27 +1,26 @@
 <!-- Jhon T. Gómez -->
-<nav class="grey lighten-2 z-depth-0" >
+  <nav class="blue-grey darken-2 z-depth-0" >
       <div class="nav-wrapper">
         <ul>
           <li>
-            <a href="#" class="white-text thin largue" style="background: #3c948b;"><?php echo "Bienvenido, " . $_SESSION["nombre"]; ?></a>
-            <!-- <a href="../Controller/cerrarsession.php" class="grey white-text thin 500" id="cerrar"><i class="material-icons right white-text">trending_down</i>Cerrar sesion</a> -->
+            <a href="#" class="white-text thin" style="background: #3c948b; width: 240px;"><?php echo "Bienvenido, " . $_SESSION["nombre"]; ?></a>
+
           </li>
-          <li>
-            <a href="#"  ><i class="material-icons button-collapse" data-activates="slide-out">menu</i></a>
-</li>
-
+          <li class="right">
+            <a href="../Controller/cerrarsession.php" class="grey darken-3 thin 500" id="cerrar"><i class="material-icons right white-text">trending_down</i>Cerrar sesion</a>
+            <!-- <a href="#"  ><i class="material-icons button-collapse" data-activates="slide-out">menu</i></a> -->
+          </li>
         </ul>
-
       </div>
     </nav>
 
 
     <ul class="side-nav fixed z-depth-0" id="mobile-demo">
-              <ul class="collapsible" data-collapsible="accordion" >
-                <!-- <li>
-                  <a href="dashboard.php?seccion=<?php echo base64_encode('inicion') ?>" class="waves-effect active" class="collapsible-header"><i class="material-icons left green-text">home</i>Inicio</a>
+              <ul class="collapsible grey lighten-4" data-collapsible="accordion" >
+                <li>
+                  <a href="dashboard.php?seccion=<?php echo base64_encode('inicion') ?>"class="collapsible-header" class="collapsible-header"><i class="material-icons left green-text">home</i>Inicio</a>
                   <div class="collapsible-body indigo-text text-lighten-5" style="background-color: #673AB7"><p>...</p></div>
-                </li> -->
+                </li>
                 <li>
                   <a href="dashboard.php?seccion=rol" class="collapsible-header"><i class="material-icons left red-text">control_point</i>Rol</a>
 
@@ -31,25 +30,23 @@
                 </li>
                 <li>
                   <a href="dashboard.php?seccion=r_propietario" class="collapsible-header"><i class="material-icons left teal-text">person_pin</i>Propietarios</a>
-                  <div class="collapsible-body white-text" style="background-color: #FF5722"><p>...</p></div>
+                  <!-- <div class="collapsible-body white-text" style="background-color: #FF5722"><p>...</p></div> -->
                 </li>
 
-
+                <li>
+                  <a href="dashboard.php?seccion=rr_producto"  class="collapsible-header"><i class="material-icons left">book</i>Asignar producto</a>
+                </li>
+                <li>
+                  <a href="dashboard.php?seccion=r_accesorio"  class="collapsible-header"><i class="material-icons left">extension</i>Accesorio</a>
+                </li>
+                <li>
+                  <a href="dashboard.php?seccion=r_permiso"  class="collapsible-header"><i class="material-icons left">pan_tool</i>Permisos</a>
+                </li>
+                <li>
+                  <a href="dashboard.php?seccion=r_modulo"  class="collapsible-header"><i class="material-icons left orange-text">view_module</i>Modulo</a>
+                </li>
                 <li>
                   <a href="dashboard.php?seccion=producto" class="collapsible-header"><i class="material-icons left indigo-text">important_devices</i>Productos</a>
-                  <div class="collapsible-body white-text" style="background-color: #FF5722"><p>...</p></div>
-                </li>
-                <li>
-                  <a href="dashboard.php?seccion=r_modulo"><i class="material-icons left orange-text">view_module</i>Modulo</a>
-                </li>
-                <li>
-                  <a href="dashboard.php?seccion=r_permiso"><i class="material-icons left">pan_tool</i>Permisos</a>
-                </li>
-                <li>
-                  <a href="dashboard.php?seccion=rr_producto"><i class="material-icons left">book</i>Asignar producto</a>
-                </li>
-                <li>
-                  <a href="dashboard.php?seccion=r_accesorio"><i class="material-icons left">extension</i>Accesorio</a>
                 </li>
               </ul>
             </ul>
