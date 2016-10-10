3d669f93516a11adc4e31cb5f3b854c33b6c6c70
JhonUser <jf29@misena.edu.co>
2016-10-10 10:55:32 -0500

    ...

--- WEBAPP/Views/form.login.php
+++ WEBAPP/Views/form.login.php
@@ -7,7 +7,7 @@
             <form class="" action="../Controller/log.controller.php" method="post">
               <input type="text" name="nombre" class="white" placeholder="Usuario" required="">
               <input type="password" name="contrasena" class="white" placeholder="Contraseña" required="">
-              <button type="submit" class="btn thin brown" name="v">Ingresar</button>
+              <button type="submit" class="btn thin brown" name="sesion">Ingresar</button>
             </form>
           </div>
         </div>
